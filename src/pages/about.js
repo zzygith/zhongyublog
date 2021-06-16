462e30ace68306b789013a0825383bac7da63fe8
Zhongyu Zhang <zhangzhongyu98@gmail.com>
2021-06-16 10:38:36 -0600

    Fix the li that shows over div

--- src/pages/about.js
+++ src/pages/about.js
@@ -45,22 +45,24 @@ const About = () => {
         <br/>
         <h2>Skills</h2>
         <hr/>
-        <br/>
-        <ul>
+          <br />
+          <div>
+        <ul style={{listStylePosition:'inside'}}>
         <li>HTML5 / CSS3 / Flexbox / Grid / BootStrap / Layui</li>
         </ul>
         <br/>
-        <ul>
+        <ul style={{listStylePosition:'inside'}}>
         <li>JavaScript / JQuery / React / Redux / Styled Component / Storybook / Gatsby / GraphQL / Git / Python</li>
-        </ul>
-        <br/>
+          </ul>
+          </div>
+          <br />
         <h2>Project</h2>
         <hr/>
         <br/>
         <h3>Instant Messaging UI</h3>
         <p><em>React &#x26; React Router &#x26; React Spring &#x26; Styled Component &#x26; Storybook</em></p>
         <p>Demo URL：<a href="https://chatui-show.netlify.app">https://chatui-show.netlify.app</a></p>
-        <ul>
+        <ul style={{listStylePosition:'inside'}}>
         <li>Used React + Hooks to develop a single-page chat application UI including 30+ components</li>
         <li>Built a three-column layout including navbar, message list and chat list by CSS-in-JS Grid, Flexbox</li>
         <li>Used React Spring to create animation and React Router to navigate</li>
@@ -70,7 +72,7 @@ const About = () => {
         <h3>Portfolio Site</h3>
         <p><em>GatsbyJS &#x26; CSS Modules &#x26; GraphQL</em></p>
         <p>Demo URL：<a href="https://zhongyublog.netlify.app/">https://zhongyublog.netlify.app/</a></p>
-        <ul>
+        <ul style={{listStylePosition:'inside'}}>
         <li>Converted my original portfolio from an HTML file to a React app using GatsbyJS and CSS Modules</li>
         <li>Used GraphQL to access markdown and other data</li>
         </ul>
@@ -78,21 +80,21 @@ const About = () => {
         <h3>Backstage Management</h3>
         <p><em>HTML &#x26; CSS &#x26; Javascript &#x26; JQuery</em></p>
         <p>Demo URL：<a href="https://zhongyublog.netlify.app/">https://zhongyublog.netlify.app/</a></p>
-        <ul>
+        <ul style={{listStylePosition:'inside'}}>
         <li>Built a basic front-end user interface that allows webmaster to add, modify and remove content</li>
         <li>Third-party APIs used to verify or change user information, create drafts or publish articles</li>
         </ul>
         <br/>
         <h3>2D Human-Pose Estimation In Realtime</h3>
         <p><em>Python &#x26; OpenCV &#x26; OpenPose</em></p>
-        <ul>
+        <ul style={{listStylePosition:'inside'}}>
         <li>Used the OpenPose algorithm to detect the pose of human</li>
         <li>Calculated the angle between Neck-Nose line and Neck-Crotch line to correct student’s sitting position</li>
         </ul>
         <br/>
         <h3>Recognition and Classification of Paper-Plastic Bags</h3>
         <p><em>Python &#x26; OpenCV &#x26; SIFT &#x26; FLANN &#x26; CART tree</em></p>
-        <ul>
+        <ul style={{listStylePosition:'inside'}}>
         <li>Used the SIFT algorithm and FLANN fast matching to achieve the classification of different bags.</li>
         <li>Detected rectangles to identify the stacking by Hough transform line detection algorithm</li>
         <li>Extracted the printing defects outline by Template Matching and classified them by CART tree</li>
